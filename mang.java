553d058248ad04ff705ca9ea82df5b5b56d676a9
lehanlong <50980111+lehanlong@users.noreply.github.com>
2019-06-06 14:45:13 +0700

    Add files via upload

---
+++ mang.java
@@ -0,0 +1,24 @@
+package baitap;
+import java.util.Scanner;
+public class mang {
+public static void main(String[] args) {
+	    int A, sum=0 , number;
+	    float avg;
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Nhập vào số phần tử của mạng: ");
+	    A = scanner.nextInt();
+	    int[] mang = new int[A];
+	    
+	    for(int i=0; i< A; i++){
+	    System.out.print("Nhập phần tử số " + i +": ");
+	    mang[i] = number = scanner.nextInt();     
+	    sum += number;
+	    }
+	    avg = (float)sum/A;
+	    System.out.println("Trung bình cộng của mạng là: " + avg);
+	    }
+
+	
+	
+
+}
